5be1b29901924823a477568dc656735832d3429e
Walt Destler <walt@waltdestler.com>
2011-07-31 18:59:07 +0000

    RGB: Some lighting and puzzle improvements.
    git-svn-id: http://svn.waltdestler.com/programming/Games/RGB@1091 d8611690-af9d-4fc2-b65e-bc122fddd6e6

---
+++ Assets/Scripts/NextLevelAction.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class NextLevelAction: MonoBehaviour
+{
+	public void DoActivateTrigger()
+	{
+		Invoke("NextLevel", 2);
+	}
+
+	private void NextLevel()
+	{
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+}
