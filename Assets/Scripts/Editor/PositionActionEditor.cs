b139259104dec3666b1d5c3f8833f8fec4de6951
Walt Destler <walt@waltdestler.com>
2011-08-01 00:23:59 +0000

    RGB: More level improvements... getting there!
    git-svn-id: http://svn.waltdestler.com/programming/Games/RGB@1094 d8611690-af9d-4fc2-b65e-bc122fddd6e6

---
+++ Assets/Scripts/Editor/PositionActionEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PositionAction))]
+public class PositionActionEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+		PositionAction pa = (PositionAction)target;
+		if(GUILayout.Button("Set Start as Current"))
+			pa.StartPos = pa.transform.position;
+		if(GUILayout.Button("Set Start as Current"))
+			pa.EndPos = pa.transform.position;
+		if(GUILayout.Button("Set Current as Start"))
+			pa.transform.position = pa.StartPos;
+		if(GUILayout.Button("Set Current as End"))
+			pa.transform.position = pa.EndPos;
+	}
+}
---
+++ Assets/Scripts/PositionAction.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PositionAction : MonoBehaviour
+{
+	public Vector3 StartPos;
+	public Vector3 EndPos;
+	public float Duration = 1;
+	public float Delay = 0;
+	public AnimationCurve Curve;
+
+	public void DoActivateTrigger()
+	{
+		StartCoroutine(RunAction());
+	}
+
+	private IEnumerator RunAction()
+	{
+		yield return new WaitForSeconds(Delay);
+		float startTime = Time.time;
+		do
+		{
+			yield return 0;
+			float t = Curve.Evaluate((Time.time - startTime) / Duration);
+			transform.position = Vector3.Lerp(StartPos, EndPos, t);
+		}
+		while(Time.time - startTime <= Duration);
+	}
+}
