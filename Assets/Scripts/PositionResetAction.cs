cde52d4a7bb0732081eec056da8d878f1c8c4f0b
Walt Destler <walt@waltdestler.com>
2011-09-14 20:10:56 +0000

    RGB: Added level 4.
    git-svn-id: http://svn.waltdestler.com/programming/Games/RGB@1149 d8611690-af9d-4fc2-b65e-bc122fddd6e6

---
+++ Assets/Scripts/PositionResetAction.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PositionResetAction : MonoBehaviour
+{
+	private Vector3 _pos;
+	private Quaternion _rot;
+	private Vector3 _vel;
+	private Vector3 _angVel;
+
+	public void Awake()
+	{
+		_pos = transform.position;
+		_rot = transform.rotation;
+		if(rigidbody != null)
+		{
+			_vel = rigidbody.velocity;
+			_angVel = rigidbody.angularVelocity;
+		}
+	}
+
+	public void DoActivateTrigger()
+	{
+		transform.position = _pos;
+		transform.rotation = _rot;
+		if(rigidbody != null)
+		{
+			rigidbody.velocity = _vel;
+			rigidbody.angularVelocity = _angVel;
+		}
+	}
+}
